9018ee430f54262234a4595ecf2f28310a9df54c
faris <faris_ddx@hotmail.com>
2024-05-14 17:35:05 +0300

    Update on 17:35 14-May-24

--- src/utils/matrixCalculation.ts
+++ src/utils/matrixCalculation.ts
@@ -1,7 +1,29 @@
-import StringToNumbers from "./stringToNumber"
+import  StringToNumbers, { createMatrixElement }from "./stringToNumber"
+
+interface Fraction {
+  num: number
+  den: number
+}
+
+const size: number = 4
 
 const ls: string[] = ["1", "3/2", "-2/2", "4"]
 
-const numls: number[][] = StringToNumbers(ls)
+const numls: Fraction[] = StringToNumbers(ls)
 
-console.log("d")
+console.log(numls)
+
+function createIdentityMatrix(size: number){
+  const SIZE = Math.sqrt(size)
+  const matrix = Array.from(Array(SIZE*SIZE).fill(createMatrixElement(0)))
+
+  const STEP = SIZE + 1 
+  for (let i = 0; i < SIZE*SIZE; i += STEP) {
+    matrix[i] = createMatrixElement(1)
+  }
+  
+  return matrix
+}
+
+
+console.log(createIdentityMatrix(size))
--- src/utils/stringToNumber.ts
+++ src/utils/stringToNumber.ts
@@ -1,16 +1,24 @@
-function StringToNumbers(list: string[]): number[][] {
-  const result = list.map((element) => {
-    if (element.length < 3) return [parseInt(element) * 1]
 
-    const [numerator, denumerator] = element
+interface Fraction {
+  num: number
+  den: number
+}
+
+export function createMatrixElement(num: number, den: number = 1): Fraction {
+  return { num, den };
+}
+
+export default function StringToNumbers(list: string[]): Fraction[] {
+  const result = list.map((element) => {
+    if (element.length < 3) return createMatrixElement(parseInt(element) * 1)
+
+    const [num, denum] = element
       .split("/")
       .map((num) => parseInt(num) * 1)
 
-    if (denumerator === 0 || denumerator === 1) return [numerator]
+    if (denum === 0 || denum === 1) return createMatrixElement(num)
 
-    return [numerator, denumerator]
+    return createMatrixElement(num, denum)
   })
   return result
 }
-
-export default StringToNumbers

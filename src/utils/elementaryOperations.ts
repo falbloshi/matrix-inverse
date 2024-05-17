b613d342b60e5dda2f3f74f41605ee124b6b7051
falbloshi <faris_ddx@hotmail.com>
2024-05-17 11:10:04 +0300

    Update on 11:10 17-May-24

--- src/utils/elementaryOperations.ts
+++ src/utils/elementaryOperations.ts
@@ -1,33 +1,43 @@
 import { Fraction } from "./types"
 
+/*
+
+*/
+
 const gcd = (a: number, b: number): number => {
   if (!b) return a
   return gcd(b, a % b)
 }
 
 const simplify = (fraction: Fraction): Fraction => {
-  const divisor = gcd(fraction.num, fraction.den)
-  return { num: fraction.num / divisor, den: fraction.den / divisor }
+  const divisor = Math.abs(gcd(fraction.num, fraction.den))
+  return {
+    num: fraction.num / divisor,
+    den: fraction.den / divisor,
+  }
 }
 
 const addition = (firstItem: Fraction, secondItem: Fraction): Fraction => {
   const first = simplify(firstItem)
   const second = simplify(secondItem)
 
-  if (first.den == 1 && second.den == 1) {
-    const add: number = first.num + second.num
-    return { num: add, den: 1 }
+  //since numerators are already negative, no need for the denum to be
+  const absFirstDen = Math.abs(first.den)
+  const absSecondDen = Math.abs(second.den)
+
+  if (absFirstDen == absSecondDen) {
+    const add = first.num + second.num
+    return simplify({ num: add, den: absSecondDen })
   } else {
-    const num: number = first.num * second.den + second.num + first.den
-    const den: number = first.den > second.den ? first.den : second.den
+    const num = first.num * absSecondDen + second.num * absFirstDen
+    const den = absFirstDen > absSecondDen ? absFirstDen : absSecondDen
+
     const result: Fraction = simplify({ num, den })
+
     return { num: result.num, den: result.den }
   }
-
-  return { num: 1, den: 1 }
 }
-
-const what: Fraction = { num: 15, den: 2 }
-const yes: Fraction = { num: 5, den: 1 }
+const what: Fraction = { num: -14, den: 2 }
+const yes: Fraction = { num: 5, den: 2 }
 
 console.log(addition(what, yes))

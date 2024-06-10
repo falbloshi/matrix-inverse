6bcba97872d0e503ae5015c45e4b1d39c013cbff
falbloshi <faris_ddx@hotmail.com>
2024-06-10 20:53:11 +0300

    Update on 20:53 10-Jun-24

--- src/components/resultPage.tsx
+++ src/components/resultPage.tsx
@@ -11,6 +11,24 @@ interface ResultPageProps {
 const ResultPage: React.FC<ResultPageProps> = ({ inputs, display }) => {
 
   const [result, setResult] = useState<any[] | null>([])
+  const [isLast, setIsLast] = useState<boolean>(false)
+  const [currentValue, setCurrentValue] = useState<number>(0)
+
+  const nextValue = () => {
+    let length = 0
+    if (result !== null) length = result.length
+    if (isLast === false) {
+      if (currentValue < length - 1) {
+        setCurrentValue(prev => prev += 1)
+      }
+      else {
+        setIsLast(true)
+      }
+    }
+    else {
+      setCurrentValue(0)
+    }
+  }
 
   useEffect(() => {
     if (display !== "pending") {
@@ -23,9 +41,12 @@ const ResultPage: React.FC<ResultPageProps> = ({ inputs, display }) => {
 
 
 
+  console.log(currentValue)
   return (
     <div>
-      <MathJax>{result ? `$$${result}$$` : ""}</MathJax>
+
+      <MathJax>{result ? `$$${result[currentValue]}$$` : ""}</MathJax>
+      <button onClick={nextValue} className="bg-black text-white px-8"> {isLast ? "Result" : "Find Next Value"}</button>
     </div>
   )
 }
--- src/utils/matrixWithRowOpsDisplay.ts
+++ src/utils/matrixWithRowOpsDisplay.ts
@@ -1,39 +1,5 @@
 import { Snapshot } from "./types"
 
-const myArray = [
-  {
-    invMatrix: ["2", "1", "4", "-3"],
-    idMatrix: ["1", "0", "0", "1"],
-    rowOps: null,
-  },
-  {
-    invMatrix: ["1", "1/2", "4", "-3"],
-    idMatrix: ["1/2", "0", "0", "1"],
-    rowOps: "R_1 \\longrightarrow \\frac{1}{2} \\cdot R_1",
-  },
-  {
-    invMatrix: ["1", "1/2", "0", "-5"],
-    idMatrix: ["1/2", "0", "-2", "1"],
-    rowOps: "R_2 \\longrightarrow R_2 - 4 \\cdot R_1",
-  },
-  {
-    invMatrix: ["1", "1/2", "0", "1"],
-    idMatrix: ["1/2", "0", "2/5", "-1/5"],
-    rowOps: "R_2 \\longrightarrow \\frac{-1}{5} \\cdot R_2",
-  },
-  {
-    invMatrix: ["1", "0", "0", "1"],
-    idMatrix: ["3/10", "1/10", "2/5", "-1/5"],
-    rowOps: "R_1 \\longrightarrow R_1 - \\frac{1}{2} \\cdot R_2",
-  },
-  {
-    originalMatrix: ["2", "1", "4", "-3"],
-    updatedIdMatrix: ["3/10", "1/10", "2/5", "-1/5"],
-    result: ["1", "0", "0", "1"],
-    rowOps: "A \\cdot A^{-1} = I",
-  },
-]
-
 import augmentedMatrixDisplay from "./augmentedMatrixDisplay"
 import resultMatrixDisplay from "./resultMatrixDisplay"
 

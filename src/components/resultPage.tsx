f2b9b6fa3e3af2d59ebe8ffc639851e37eb0e47d
falbloshi <faris_ddx@hotmail.com>
2024-06-11 06:16:45 +0300

    Update on 06:16 11-Jun-24

--- src/components/resultPage.tsx
+++ src/components/resultPage.tsx
@@ -10,14 +10,15 @@ interface ResultPageProps {
 
 const ResultPage: React.FC<ResultPageProps> = ({ inputs, display }) => {
 
-  const [result, setResult] = useState<any[] | null>([])
+  const [result, setResult] = useState<string[] | null>([])
   const [isLast, setIsLast] = useState<boolean>(false)
+  const [displayResult, setDisplayResult] = useState<JSX.Element[]>([])
   const [currentValue, setCurrentValue] = useState<number>(0)
 
   const nextValue = () => {
-    if (!result) return
+    if (!result || currentValue >= result.length - 1) return;
 
-    setCurrentValue(prev => prev += 1)
+    setCurrentValue(prev => prev + 1)
 
     if (currentValue == result.length - 2) {
       setIsLast(true)
@@ -30,19 +31,29 @@ const ResultPage: React.FC<ResultPageProps> = ({ inputs, display }) => {
   useEffect(() => {
     if (display !== "pending") {
       setResult(_prevResult => matrixWithRowOpsDisplay(gaussElimination(inputs) as any[]))
+      setCurrentValue(0)
+      setDisplayResult([])
+      setIsLast(false)
     }
     else {
       setResult(null)
     }
-  }, [display])
+  }, [display, inputs])
 
 
-  if (result !== null) console.log(result.length)
-  console.log(`curr val`, currentValue)
+  useEffect(() => {
+    if (result !== null) {
+      setDisplayResult(prev => [...prev,
+      <div key={currentValue}>
+        <MathJax>{`$$${result[currentValue]}$$`}</MathJax>
+      </div>])
+    }
+  }, [currentValue, result])
+
   return (
     <div>
-
       <MathJax>{result ? `$$${result[currentValue]}$$` : ""}</MathJax>
+      {displayResult}
       <button onClick={nextValue} className="bg-black text-white px-8"> {isLast ? "Result" : "Find Next Value"}</button>
     </div>
   )

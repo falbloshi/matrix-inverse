6e98e7dff5ed3631bd241ce5438f8a38d4a97922
falbloshi <faris_ddx@hotmail.com>
2024-08-10 11:42:43 +0300

    Update on 11:42 10-Aug-24

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+current_branch=$(git symbolic-ref --short HEAD)
+
 git add --all 
 
 dtm=`date +"%H:%M %d-%b-%y"`
 
 git commit -m "Update on ${dtm}"
-git push -u origin master
+git push -u origin "${current_branch}"

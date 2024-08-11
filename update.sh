b23ecbb537818f3038f3c4b34744029e8eb53431
falbloshi <faris_ddx@hotmail.com>
2024-08-11 20:47:19 +0300

    Update on 20:47 11-Aug-24

--- update.sh
+++ update.sh
@@ -9,4 +9,4 @@ dtm=`date +"%H:%M %d-%b-%y"`
 git commit -m "Update on ${dtm}"
 git push -u origin "${current_branch}"
 
-echo "\033[32mUpdated on ${dtm} and pushed to ${current_branch}"
+echo "\e[32mUpdated on ${dtm} and pushed to ${current_branch}"
